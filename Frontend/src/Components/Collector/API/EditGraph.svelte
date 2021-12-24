c4a450ef0e743142475b348b8c9c88dcdb8d2ea0
DoKM <mcazzyman@gmail.com>
2021-12-24 09:19:47 +0100

    Fix typo

--- Frontend/src/Components/Collector/API/EditGraph.svelte
+++ Frontend/src/Components/Collector/API/EditGraph.svelte
@@ -149,7 +149,7 @@
         on:click={() => update()}
         class="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-green-500 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
       >
-        Create
+        Edit
       </button>
       </span>
   </Modal>
